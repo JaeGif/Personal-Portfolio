2123ebdf3309dc3bd0959cd9135d6de01cae513f
Jae <giffordjacob0@gmail.com>
2022-11-08 15:31:43 +0800

    parallaxProject JS effects refacored for maintainability

--- src/parallaxProjects.js
+++ src/parallaxProjects.js
@@ -1,32 +1,33 @@
 function projectParallax() {
   const projectCard = document.getElementsByClassName('positional-wrapper');
+
+  window.addEventListener('scroll', () => {
+    projectSectionFocused();
+    for (let i = 0; i < projectCard.length; i++) {
+      if (isElementInViewport(projectCard[i])) {
+        parallaxMotionOfAccentAndDescription(i);
+      }
+    }
+  });
+}
+
+function parallaxMotionOfAccentAndDescription(index) {
   const descriptionCard = document.getElementsByClassName('description-card');
   const underImageAccent = document.getElementsByClassName(
     'under-image-outline'
   );
   const descriptionCardVelocity = 0.25;
   const accentVelocity = 0.055;
-  const projectEntireArea =
-    document.getElementsByClassName('heading-section')[0];
-
-  window.addEventListener('scroll', () => {
-    let scrollY = window.scrollY;
-    const windowHeight = window.innerHeight;
-    projectSectionFocused();
-    for (let i = 0; i < projectCard.length; i++) {
-      if (isElementInViewport(projectCard[i])) {
-        const initialDescriptionCardPosition = -200 - windowHeight * 0.2 * i;
-        const initialUnderImageAccentPosition = -50 - i * 29;
-        underImageAccent[i].style.top =
-          initialUnderImageAccentPosition + scrollY * accentVelocity + 'px';
-        descriptionCard[i].style.bottom =
-          initialDescriptionCardPosition +
-          scrollY * descriptionCardVelocity +
-          'px';
-      }
-    }
-  });
+  let scrollY = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const initialDescriptionCardPosition = -200 - windowHeight * 0.2 * index;
+  const initialUnderImageAccentPosition = -50 - index * 29;
+  underImageAccent[index].style.top =
+    initialUnderImageAccentPosition + scrollY * accentVelocity + 'px';
+  descriptionCard[index].style.bottom =
+    initialDescriptionCardPosition + scrollY * descriptionCardVelocity + 'px';
 }
+
 function isElementInViewport(el) {
   let rect = el.getBoundingClientRect();
   return rect.top >= -375 && rect.bottom <= window.innerHeight + 375;
@@ -41,27 +42,39 @@ function isProjectInViewport(el) {
     bottomBound >= -550
   );
 }
+
 function projectSectionFocused() {
-  const body = document.body;
   const descriptionCard = document.getElementsByClassName('description-card');
-  const navBar = document.getElementById('nav-container');
-  const progressBar = document.getElementById('progress-container');
+
   const projectEntireArea =
     document.getElementsByClassName('heading-section')[0];
   if (isProjectInViewport(projectEntireArea)) {
+    projectsShownEffects();
     for (let i = 0; i < descriptionCard.length; i++) {
       descriptionCard[i].classList.add('description-in-focus');
     }
-    body.classList.add('in-focus');
-    navBar.classList.remove('uncolorNav');
-    navBar.classList.add('nav-highlight');
-    transformContactSectionBack();
-    progressBar.style.display = 'flex';
   } else {
-    body.classList.remove('in-focus');
-    navBar.classList.add('uncolorNav');
-    navBar.classList.remove('nav-highlight');
+    projectsOffScreenEffects();
   }
+}
+
+function projectsShownEffects() {
+  const body = document.body;
+  const navBar = document.getElementById('nav-container');
+  const progressBar = document.getElementById('progress-container');
+
+  body.classList.add('in-focus');
+  navBar.classList.remove('uncolorNav');
+  navBar.classList.add('nav-highlight');
+  transformContactSectionBack();
+  progressBar.style.display = 'flex';
+}
+function projectsOffScreenEffects() {
+  const body = document.body;
+  const navBar = document.getElementById('nav-container');
+  body.classList.remove('in-focus');
+  navBar.classList.add('uncolorNav');
+  navBar.classList.remove('nav-highlight');
 }
 function transformContactSectionBack() {
   const contactCallToAction = document.getElementsByClassName(
@@ -74,4 +87,5 @@ function transformContactSectionBack() {
   contactForm.classList.add('hidden');
   contactForm.classList.remove('show');
 }
+
 export default projectParallax;
